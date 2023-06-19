e4421bdb35908348a1db37dbe8c83b2e6cb7ca44
Bendegúz Takács <bendeguz.takacs@gmail.com>
2023-06-19 23:58:08 +0200

    init react app

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="header-text">
+            <span className="top-line">Hi. My name is</span>
+            <span className="middle-line">Bendegúz Takács</span>
+            <span className="bottom-line">I build software.</span>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
