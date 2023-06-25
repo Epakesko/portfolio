d2882c037befe4e7aff3bbf88ba1eaf4ae7e8e76
Bendeguz <takacs.bendeguz@webtown.hu>
2023-06-25 23:46:42 +0200

    init AboutMe component, format files

--- src/components/Banner.component.tsx
+++ src/components/Banner.component.tsx
@@ -1,13 +1,13 @@
-import './Banner.styles.scss';
+import "./Banner.styles.scss";
 
 const Banner = () => {
-    return (
-        <div className="header-text">
-            <span className="top-line">Hi. My name is</span>
-            <span className="middle-line">Bendegúz Takács</span>
-            <span className="bottom-line">I bring imagination to reality through code.</span>
-        </div>
-    )
-}
+  return (
+    <div className="header-text">
+      <span className="top-line">Hi. My name is</span>
+      <span className="middle-line">Bendegúz Takács</span>
+      <span className="bottom-line">I bring imagination to reality through code.</span>
+    </div>
+  );
+};
 
 export default Banner;
