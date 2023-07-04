2fcbffe383cded627c97f57948510a8f6d30f83f
Bendegúz Takács <bendeguz.takacs@gmail.com>
2023-07-04 02:52:15 +0200

    add KBC slider

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import EsgImage from "./media/msci/esg.png";
 import FacsImage from "./media/msci/facs.png";
 import SerchImage from "./media/msci/search.png";
 import ResearchForm from "./media/msci/research-form.png";
+import Fragments from "./media/kbc/fragments.png";
+import LTV from "./media/kbc/ltv_calculator.png";
+import Mortgage from "./media/kbc/mortgage_calculator.png";
 
 function App() {
   return (
@@ -18,7 +21,7 @@ function App() {
         <Banner />
         <AboutMe />
         <Slider
-          imageSliderOnRight={true}
+          imageSliderOnRight={false}
           title="MSCI"
           description="MSCI is a leading financial company. Worked with them for 3 years on various different aspects of their portal."
           items={[
@@ -49,31 +52,25 @@ function App() {
           ]}
         />
         <Slider
-          imageSliderOnRight={false}
-          title="MSCI"
-          description="MSCI is a leading financial company. Worked with them for 3 years on various different aspects of their portal."
+          imageSliderOnRight={true}
+          title="KBC"
+          description="KBC was an irish bank, but sadly they were purchages by Bank of Ireland and the website was discontinued."
           items={[
             {
-              image: EsgImage,
-              title: "ESG Ratings",
+              image: Fragments,
+              title: "Responsive elements",
               description:
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse urna justo, lobortis sed urna sed, convallis dictum sapien. Maecenas sit amet porta ligula.",
             },
             {
-              image: FacsImage,
-              title: "FaCS Report",
+              image: LTV,
+              title: "Calculators",
               description:
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse urna justo, lobortis sed urna sed, convallis dictum sapien. Maecenas sit amet porta ligula.",
             },
             {
-              image: SerchImage,
-              title: "Dynamic search",
-              description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse urna justo, lobortis sed urna sed, convallis dictum sapien. Maecenas sit amet porta ligula.",
-            },
-            {
-              image: ResearchForm,
-              title: "Research form",
+              image: Mortgage,
+              title: "Calculators",
               description:
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse urna justo, lobortis sed urna sed, convallis dictum sapien. Maecenas sit amet porta ligula.",
             },
